9695422cd53e96c2643740d37be2468d9053948e
Harrison <harrisonch2023@gmail.com>
2026-06-25 23:48:08 -0700

    API logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,93 @@
 import { useState } from 'react'
 import './App.css'
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
   //This reduces calls as certain topics don't have certain keywords.
-  const categories = [ // Specific category to add
-    ["Artemis", "ISS", "Apollo", "Gemini", "Skylab"], //MANNED SPACECRAFTS 
-    ["Hubble", "JWST", "Chandra", "DART", "New Horizons", "Voyager", "Pioneer", "Cassini"], //UNMANNED SPACECRAFTS
-    ["Planet", "Star", "Blackhole", "Exoplanet", "Moon", "Titan", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto", "Nebula", "Asteroid"]
+
+  const categories = [
+    {name: "Artemis", type: 0}, {name: "ISS", type: 0}, {name: "Apollo", type: 0}, {name: "Gemini", type: 0}, {name: "Skylab", type: 0},
+    {name: "Hubble", type: 1}, {name: "JWST", type: 1}, {name: "Chandra", type: 1}, {name: "Double Asteroid Redirection Test", type: 1}, {name: "New Horizons", type: 1}, 
+    {name: "Voyager", type: 1}, {name: "Pioneer", type: 1}, {name: "Cassini", type: 1}, {name: "Planet", type: 2}, {name: "Star", type: 2}, 
+    {name: "Blackhole", type: 2}, {name: "Exoplanet", type: 2}, {name: "Moon", type: 2}, {name: "Titan", type: 2}, {name: "Mercury", type: 2}, 
+    {name: "Venus", type: 2}, {name: "Earth", type: 2}, {name: "Mars", type: 2}, {name: "Jupiter", type: 2}, {name: "Saturn", type: 2}, 
+    {name: "Uranus", type: 2}, {name: "Neptune", type: 2}, {name: "Pluto", type: 2}, {name: "Nebula", type: 2}, {name: "Asteroid", type: 2}, {name: "Galaxy", type: 2}
   ]
 
   const modifiers= [ // Specific keyword modifiers
-    ["EVA", "Research", "Diagram", "Crew", "Astronauts", "Exploration", "Mission", "Experiment"],
-    ["Research", "Diagram", "Experiment", "Exploration", "Mission", "Technology"],
-    ["Telescope", "Illustration", "Research", "Experiment"]
+    ["EVA", "Research", "Diagram", "Crew", "Astronauts", "Exploration", "Mission", "Experiment"], //MANNED SPACECRAFTS
+    ["Research", "Diagram", "Experiment", "Exploration", "Mission", "Technology"], //UNMANNED SPACECRAFTS
+    ["Telescope", "Illustration", "Research", "Experiment"] //CELESTIAL OBJECTS
   ]
+
+  const [currImage, setCurrImage] = useState("none");
+  const [currDesc, setCurrDesc] = useState("");
+  const [banned, setBanned] = useState([]);
+  const [start, setStart] = useState(false);
+
+
+  const beginApp = () => {
+    setStart(true);
+    newQuery();
+  }
+
+  const newQuery = (e) => {
+    if (e != null) {e.preventDefault();}
+    const keywords = prepQuery();
+    let str = keywords.join(", ");
+    const query = `https://images-api.nasa.gov/search?keywords=${str}&media_type=image`
+    callAPI(query);
+  }
+
+  const callAPI = async (q) => {
+    const response = await fetch(q);
+    const json = await response.json();
+    if (json == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+    } else {
+      setCurrentImage(json);
+    }
+  }
+
+  const setCurrentImage = (j) => {
+    const randIndex = Math.floor(Math.random() * j.collection.items.length);
+    setCurrImage(j.collection.items[randIndex].links[0].href)
+    setCurrDesc(j.collection.items[randIndex].data[0].description);
+  }
+
+  const prepQuery = () => {
+    let allowedCategories = [];
+    let allowedModifiers = [];
+    let queryItems = [];
+    for (const value of categories) {
+      if (!banned.includes(value)) {
+        allowedCategories.push(value);
+      }
+    }
+
+    let randCategories = Math.floor(Math.random() * allowedCategories.length);
+    let choosenCategory = allowedCategories[randCategories];
+    let choosenModifier = modifiers[choosenCategory.type];
+    queryItems.push(choosenCategory.name);
+    for (const value of choosenModifier) {
+       if (!banned.includes(value)) {
+        allowedModifiers.push(value);
+       }
+    }
+
+    for (let i = 0; i < allowedModifiers.length; i++) {
+      let randModifiers = Math.floor(Math.random() * allowedModifiers.length);
+      queryItems.push(allowedModifiers[randModifiers]);
+    }
+
+    return queryItems;
+  }
+
+
+
+  const started = (p) => ({
+    display: p ? "none" : "flex"
+  }); 
 
   return (
     <div className="App">
@@ -25,21 +99,22 @@ function App() {
           
         </div>
       </div>
-      <div className='interactable container'>
+      <div className='interactable container' >
         <div className='titleCard'>
           <h1>NASA Images</h1>
           <h2>A random selection of NASA images to gaze into the stars with.</h2>
         </div>
-        <div className="content">
-          <img></img>
-          <form>
+        <button className='launchButton' style={started(start)} onClick={beginApp}> Launch! </button>
+        <div className="content" style={started(!start)}>
+          <img src={currImage}></img>
+          <form onSubmit={newQuery}>
             <div className='attributes'>
               {/* buttons here */}
             </div>
             <input type='submit' value="Roll Another"></input>
           </form>
           <h2 className='descriptionH2'>Description</h2>
-          <p className='description'></p>
+          <p className='description'>{currDesc}</p>
         </div>
       </div>
       <div className='historyContainer container'>
